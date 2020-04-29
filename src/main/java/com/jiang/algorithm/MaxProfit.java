79439f3cbe7a70c9670d2498d6d28b2d5e4fc3fb
newjiang <newjiang@tom.com>
2020-04-29 17:47:53 +0800

    MaxProfit

---
+++ src/main/java/com/jiang/algorithm/MaxProfit.java
@@ -0,0 +1,39 @@
+package com.jiang.algorithm;
+
+/**
+ * @author: newjiang
+ * @date: 2020/4/29 17:32
+ * @description: todo  买卖股票的最佳时机
+ * 例如数据右边所列举:  7,  1,  5,  3,  6,  4
+ * 转化为每天的涨跌 =>   -6  +4  -2  +3  -2
+ * 即[7, 1, 5, 3, 6, 4] => [-6, +4, -2, +3, -2]
+ * 最后的问题是求数组最大的连续子集[-6, +4, -2, +3, -2] => [+4, -2, +3]
+ **/
+public class MaxProfit {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{};
+        System.out.println(new MaxProfit().maxProfit(arr));
+    }
+
+    public int maxProfit(int[] prices) {
+
+        if (prices.length < 2) {
+            return 0;
+        }
+
+        int[] temp = new int[prices.length - 1];
+        for (int i = 1; i < prices.length; i++) {
+            temp[i - 1] = prices[i] - prices[i - 1];
+        }
+
+        int max = temp[0];
+        for (int i = 1; i < temp.length; i++) {
+            if (temp[i - 1] > 0) {
+                temp[i] += temp[i - 1];
+            }
+            max = Math.max(temp[i], max);
+        }
+        return max > 0 ? max : 0;
+    }
+}
